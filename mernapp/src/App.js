1e6466bb38669f5e017daa04ba8e8e0738cfaa11
Umer-Qadir <umerqadir113@gmail.co>
2023-09-17 09:36:48 -0700

    changes in package.json

--- mernapp/src/App.js
+++ mernapp/src/App.js
@@ -10,18 +10,21 @@ import {
 import '../node_modules/bootstrap-dark-5/dist/css/bootstrap-dark.min.css';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
+import { CartProvider } from './components/ContextReducer';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route exact path="/login" element={<Login />} />
-          <Route exact path="/signup" element={<Signup />} />
-        </Routes>
-      </div>
-    </Router>
+    <CartProvider>
+      <Router>
+        <div>
+          <Routes>
+            <Route exact path="/" element={<Home />} />
+            <Route exact path="/login" element={<Login />} />
+            <Route exact path="/signup" element={<Signup />} />
+          </Routes>
+        </div>
+      </Router>
+    </CartProvider>
   );
 }
 
